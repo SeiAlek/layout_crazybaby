e4944012e5ccd4dab63affd04ac4c00f691c8b5b
Oleksii Mykhailov <alem.coding@gmail.com>
2020-03-20 18:23:32 +0200

    Created main page - 90 percent

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const btnMenuHeader = document.getElementById('header__menu-toggler');
+const menuHeader = document.getElementById('header__menu');
+
+btnMenuHeader.addEventListener('click', () => {
+  menuHeader.classList.toggle('header__menu--show');
+  btnMenuHeader.classList.toggle('header__menu-toggler--cross');
+});
