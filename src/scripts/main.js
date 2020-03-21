29ced51585add2d7ede2fc82e703c232db141b11
Oleksii Mykhailov <alem.coding@gmail.com>
2020-03-21 18:50:25 +0200

    Main page done completely

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,27 @@
 
 const btnMenuHeader = document.getElementById('header__menu-toggler');
 const menuHeader = document.getElementById('header__menu');
+const nav = document.getElementById('nav');
 
 btnMenuHeader.addEventListener('click', () => {
   menuHeader.classList.toggle('header__menu--show');
   btnMenuHeader.classList.toggle('header__menu-toggler--cross');
+  nav.classList.toggle('nav--show');
+});
+
+const techBlock = document.getElementById('technology__container');
+const navSliderTech = document.getElementById('technology__slider-nav');
+const prevSlideTech = document.getElementById('technology-slider--prev');
+const nextSlideTech = document.getElementById('technology-slider--next');
+
+navSliderTech.addEventListener('click', (e) => {
+  event.preventDefault();
+
+  if (e.target === prevSlideTech) {
+    techBlock.classList.remove('technology__container--right');
+  }
+
+  if (e.target === nextSlideTech) {
+    techBlock.classList.add('technology__container--right');
+  }
 });
