892e0f6734765f336a4217c82e51bbd6b593dcd7
Oleksii Mykhailov <alem.coding@gmail.com>
2020-03-24 11:45:40 +0200

    Updates

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,18 +1,44 @@
 'use strict';
 
-const itemQty = document.querySelector('#product__qty');
-const qtyMinus = itemQty.querySelector('#product__qty-minus');
-const qtyPlus = itemQty.querySelector('#product__qty-plus');
-const qtyField = itemQty.querySelector('#qty-field');
+const product = document.querySelector('#product');
 
-itemQty.addEventListener('click', (e) => {
+const productList = document.querySelector('#product-list');
+const btnDelete = productList.querySelectorAll('.product-list__item-delete');
+
+const qtyProduct = product.querySelector('.quantity');
+const qtyMinus = qtyProduct.querySelector('.quantity__button--minus');
+const qtyPlus = qtyProduct.querySelector('.quantity__button--plus');
+const qtyField = qtyProduct.querySelector('.form__field-qty');
+
+qtyProduct.addEventListener('click', (e) => {
   changeQty(e);
 });
 
-itemQty.addEventListener('keydown', (e) => {
+qtyProduct.addEventListener('keydown', (e) => {
   if (event.code === 'Space') {
     changeQty(e);
   }
+});
+
+// btnAddProduct.addEventListener('click', () => {
+//   const item = document.createElement('li');
+//   const btnDelete = document.createElement('div');
+
+//   item.classList.add('product-list__item');
+//   btnDelete.classList.add('product-list__item-delete');
+//   item.append('');
+//   item.append(productQty);
+//   item.append(btnDelete);
+
+//   productList.append(item);
+// });
+
+productList.addEventListener('click', (e) => {
+  btnDelete.forEach((item, i) => {
+    if (e.target === item) {
+      item.parentElement.remove();
+    }
+  });
 });
 
 const changeQty = (e) => {
